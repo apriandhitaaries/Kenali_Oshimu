f91582361d6d5cd2ff8f0d807e9f1745d9c9755c
Apriandhita Aries Prayoga <apriandhitaaries@gmail.com>
2024-03-06 23:48:03 +0700

    Update minimumSDK and rebuild the APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.waro.aku"
-        minSdk = 30
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
